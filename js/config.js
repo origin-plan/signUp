9af70f7cbdb827b9955413b4fd1f6ee6927c355e
艾尔尼亚-A <1173197065@qq.com>
2017-04-20 13:19:13 +0800

    修改配置

--- js/config.js
+++ js/config.js
@@ -1,5 +1,5 @@
-var ip = '127.0.0.1:80';
-localStorage.urlIP = "http://" + ip + "/background/";
+var ip = '127.0.0.1:80/signUpAdmin/';
+localStorage.urlIP = "http://" + ip + "userLogin.php";
 
 function getUrlIp() {
 	return localStorage.urlIP;
