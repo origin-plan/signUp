165f531c8572292661bb76c34d33a466aff3a706
Hail9527Hydra <yankaimail@qq.com>
2017-04-21 02:12:10 +0800

    修改小参数

--- js/config.js
+++ js/config.js
@@ -9,3 +9,21 @@ function getUrlIp() {
 function getSetInfoUrl() {
 	return "http://127.0.0.1/signUpAdmin/setInfo.php";
 }
+
+function setUserID(userID) {
+	localStorage.userID = userID;
+
+}
+
+function setUserPwd(userPwd) {
+	localStorage.userPwd = userPwd;
+}
+
+function getUserID() {
+	return localStorage.userID;
+
+}
+
+function getUserPwd() {
+	return localStorage.userPwd;
+}
