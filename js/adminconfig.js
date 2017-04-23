fc7e9c1b649946682aa1741739b9a136a889c5fe
Hail9527Hydra <yankaimail@qq.com>
2017-04-23 18:33:44 +0800

    修改参数

---
+++ js/adminconfig.js
@@ -0,0 +1,15 @@
+function setadminID(adminID) {
+	localStorage.adminID = adminID;
+}
+
+function setadminPwd(adminPwd) {
+	localStorage.adminPwd = adminPwd;
+}
+
+function getadminID() {
+	return localStorage.adminID;
+}
+
+function getadminPwd() {
+	return localStorage.adminPwd;
+}
